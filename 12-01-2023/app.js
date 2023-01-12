0f276d776cbb0f14433e4f12946e5d8903a4f322
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-12 17:12:53 +0500

    12-01-2023

---
+++ 12-01-2023/app.js
@@ -0,0 +1,23 @@
+
+// let i=0
+
+// while(i<=10){
+// alert(i)
+// i++;
+// }
+
+// let i=11
+// do{
+//     alert(i)
+//     i++
+// }while(i<=10)
+
+
+// let stdData=["Ali","Wasay","Hassan"]
+// // alert(stdData[0]+" "+stdData[1])
+// // alert(stdData.length)
+// for(let i=0;i<stdData.length;i++){
+//     alert(stdData[i])
+// }
+let stdData=["Ali",[1,[4,5,6],3],"Hassan"]
+alert(stdData[1][1][1])
